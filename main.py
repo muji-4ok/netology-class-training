1fe68f41e69892ca99373c990f966848dde617b6
muji-4ok <33330656+muji4ok@users.noreply.github.com>
2017-12-21 22:30:10 +0300

    Initial commit 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+class Animal():
+    phrase = 'Not implemented'
+
+    def __init__(self, name, gender, phrase, bio_class):
+        self.phrase = phrase
+        self.name = name
+        self.gender = gender
+        self.bio_class = bio_class
+
+    def speak(self):
+        class_name = type(self).__name__.lower()
+        phrase = '"{}"'.format(self.phrase)
+        print('{} the {} says {}'.format(self.name, class_name, phrase))
+
+
+class Cow(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Moo', 'mammal')
+
+
+class Goat(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Baaaa', 'mammal')
+
+
+class Sheep(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Baaaa', 'mammal')
+
+
+class Pig(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Oink-oink', 'mammal')
+
+
+class Duck(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Quack', 'bird')
+
+    def speak(self):
+        gender = 'he' if self.gender == 'male' else 'she'
+        class_name = type(self).__name__.lower()
+        phrase = '"{}"'.format(self.phrase)
+        print('{} is a {} and {} says {}'.format(self.name, class_name,
+                                                 gender, phrase))
+
+
+class Chicken(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Cock-a-doodle-doo', 'bird')
+
+
+class Goose(Animal):
+    def __init__(self, name, gender):
+        super().__init__(name, gender, 'Quack', 'bird')
